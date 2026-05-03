d8005e823009af6fd6340da327db35b0440cc1e4
Enver Fransazov <enverfransazov@gmail.com>
2026-05-03 16:28:51 +0300

    commit

--- Playwright-Inrtoduction/tests/todo.test.js
+++ Playwright-Inrtoduction/tests/todo.test.js
@@ -13,9 +13,25 @@ test("user can add task", async ({page}) =>{
 
     //assert
     const taskText = await page.textContent('.task');
+    expect(taskText).toContain('Test task');
 });
 
-test("user can delete task", async ({page}) =>{});
+//Verify user can delete task
+test("user can delete task", async ({page}) =>{
+    //arrange
+    await page.goto('http://localhost:8080');
+
+    //act
+    await page.fill('#task-input', 'Test task 1');
+    await page.click('#add-task');
+    await page.click('.task .delete-task');
+
+    //assert
+    const tasks = await page.$$eval('.task', tasks => tasks.map(
+        task => task.textContent
+    ));
+    expect(tasks).not.toContain('Test task 1');
+});
 
 test("user can mark task as complete", async ({page}) =>{});
 
