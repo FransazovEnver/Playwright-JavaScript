2f3f58c15e8f16b923145d40ad7073e58793adb5
Enver Fransazov <enverfransazov@gmail.com>
2026-05-04 11:11:34 +0300

    first test link is visible

--- Playwright-Lab/Library-Catalog/tests/library.UItest.js
+++
@@ -1,6 +0,0 @@
-const {test, expect} = require('@playwright/test')
-
-//Verify That the "All Books" Link Is Visible
-test('Verify That the "All Books" Link Is Visible', async ({page}) =>{
-    //
-})
---
+++ Playwright-Lab/Library-Catalog/tests/ui.test.js
@@ -0,0 +1,16 @@
+const {test, expect} = require('@playwright/test')
+
+//Verify That the "All Books" Link Is Visible
+test('Verify That the "All Books" Link Is Visible', async ({page}) =>{
+    
+    //arrange
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('nav.navbar');
+
+    //act
+    const allBooksLink = page.locator("//a[@href='/catalog']");
+    const isLinkVisible = await allBooksLink.isVisible();
+
+    //assert
+    expect(isLinkVisible).toBe(true);
+})
