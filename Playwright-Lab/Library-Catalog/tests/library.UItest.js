d381e2e15b81624614f8711f9da36d9fd9ffa478
Enver Fransazov <enverfransazov@gmail.com>
2026-05-04 10:56:15 +0300

    commit the app files

---
+++ Playwright-Lab/Library-Catalog/tests/library.UItest.js
@@ -0,0 +1,6 @@
+const {test, expect} = require('@playwright/test')
+
+//Verify That the "All Books" Link Is Visible
+test('Verify That the "All Books" Link Is Visible', async ({page}) =>{
+    //
+})
