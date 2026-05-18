c78b4266009a59bd97c7872eb0a91c9d4ef61cf3
Enver Fransazov <enverfransazov@gmail.com>
2026-05-18 11:08:49 +0300

    commit

---
+++ PracticalProject-AutomatedTestingwithPlaywright/test/e2e.test.js
@@ -0,0 +1,109 @@
+const { test, describe, beforeEach, afterEach, beforeAll, afterAll, expect } = require('@playwright/test');
+const { chromium } = require('playwright');
+
+const host = 'http://localhost:3000/';
+
+let browser;
+let context;
+let page;
+
+let user = {
+    email: "",
+    password: "123456",
+    confirmPass: "123456"
+};
+
+let game = {
+    title: "",
+    category: "",
+    id: "",
+    maxLevel: "99",
+    imageUrl: "SomeImage",
+    summary: "Latest Diablo version",
+    id: ""
+}
+
+describe("e2e tests", () => {
+    beforeAll(async () => {
+        browser = await chromium.launch();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    beforeEach(async () => {
+        context = await browser.newContext();
+        page = await context.newPage();
+    });
+
+    afterEach(async () => {
+        await page.close();
+        await context.close();
+    });
+
+describe("Authentication Tests", () => {
+    test("Register with valid data", async () => {
+        
+        await page.goto(host);
+        await page.click("//a[@href='/register']");
+        await page.waitForSelector("form");
+
+        let random = Math.floor(Math.random() * 1000);
+        user.email = `abv_${random}@abv.bg`;
+
+        await page.fill("//input[@id='email']", user.email);
+        await page.fill("//input[@id='register-password']", user.password);
+        await page.fill("//input[@id='confirm-password']", user.confirmPass);
+        await page.click("//input[@type='submit']");
+
+        await expect(page.locator("//a[@href='/logout']")).toBeVisible();
+        expect(page.url()).toBe(host);
+    })
+
+    test("Register does not work with empty fields", async () => {
+        await page.goto(host);
+        await page.click("//a[@href='/register']");
+        await page.waitForSelector("form");
+
+        page.on('dialog', async dialog =>{
+            expect(dialog.message()).toBe("No empty fields are allowed and confirm password has to match password!");
+            await dialog.accept();
+        });
+        
+        await page.click("//input[@type='submit']");
+        expect(page.url()).toBe(host +"register");
+    });
+
+    test("Login with valid credentials", async () => {
+        await page.goto(host);
+        await page.click("//a[text()='Login']");
+        await page.waitForSelector('form');
+
+        await page.fill("//input[@id='email']", user.email);
+        await page.fill("//input[@id='login-password']", user.password);
+        await page.click("//input[@type='submit']");
+
+        await expect(page.locator("//a[@href='/logout']")).toBeVisible();
+        expect(page.url()).toBe(host);
+
+
+    })
+
+});
+
+describe("Navigation Bar Tests", () => {
+
+});
+
+describe("CRUD Operations Tests", () => {
+
+});
+
+describe("Home Page Tests", () => {
+
+});
+
+
+
+});
