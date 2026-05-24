58598226d544361d2d6773bea1434d0b50acd34c
Enver Fransazov <enverfransazov@gmail.com>
2026-05-24 22:47:39 +0300

    commit

--- Playwright-ExamPreparation 20.06.24/tests/e2e.test.js
+++ Playwright-ExamPreparation 20.06.24/tests/e2e.test.js
@@ -1,5 +1,5 @@
 const {test, describe, beforeEach, afterEach, beforeAll, afterAll, expect } = require('@playwright/test');
-const { chromium } = require('@playwright');
+const { chromium } = require('playwright');
 
 const host = 'http://localhost:3000/';
 
@@ -20,3 +20,51 @@ let meme = {
     imageUrl: "Some Image"
 }
 
+describe("e2e tests", () => {
+    beforeAll(async () => {
+        browser = await chromium.launch();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    beforeEach(async () => {
+        context = await browser.newContext();
+        page = await context.newPage();
+    });
+
+    afterEach(async () => {
+        await page.close();
+        await context.close();
+    });
+
+    describe("authentication", () =>{
+       test("Registration with Valid Data", async() =>{
+            await page.goto(host)
+            await page.click("//div[@class='guest']//a[text()='Register']");
+            await page.waitForSelector('form');
+
+       });
+       
+        test("Login with Valid Data", async() => {
+            await page.goto(host);
+        });
+
+        test("Logout from the Application", async() =>{
+
+        });
+
+    });
+
+    describe("navbar", () =>{
+
+    });
+
+    describe("CRUD", () =>{
+
+    });
+
+
+
+});
