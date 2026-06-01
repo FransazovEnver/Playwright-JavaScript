fdf647ec8bb06c785913b535f9c97a3904028c12
Enver Fransazov <enverfransazov@gmail.com>
2026-06-01 12:04:53 +0300

    first exercise

--- Playwright-ExamPreparation 11.02.25/01.DOM-Manipulation-App/app.js
+++ Playwright-ExamPreparation 11.02.25/01.DOM-Manipulation-App/app.js
@@ -1,5 +1,22 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    // TODO...
+    let numberOfTicketsInput = document.getElementById("num-tickets");
+    let seatingPreferenceInput = document.getElementById("seating-preference");
+    let fullNameInput = document.getElementById("full-name");
+    let emailInput = document.getElementById("email");
+    let phoneNumberInput = document.getElementById("phone-number");
+
+    let purchaseTicketButton = document.getElementById("purchase-btn");
+
+    purchaseTicketButton.addEventListener("click", printValues)
+
+    function printValues () {
+        console.log(numberOfTicketsInput.value);
+        console.log(seatingPreferenceInput.value);
+        console.log(fullNameInput.value);
+        console.log(emailInput.value);
+        console.log(phoneNumberInput.value);
+    }
+
 }
