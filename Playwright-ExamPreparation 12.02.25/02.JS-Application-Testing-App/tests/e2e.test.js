b9847d92abe261ced8e395337338ab512c497a44
Enver Fransazov <enverfransazov@gmail.com>
2026-06-09 17:22:43 +0300

    commit a setups in playwright tests

--- Playwright-ExamPreparation 12.02.25/02.JS-Application-Testing-App/tests/e2e.test.js
+++ Playwright-ExamPreparation 12.02.25/02.JS-Application-Testing-App/tests/e2e.test.js
@@ -34,4 +34,45 @@ describe("e2e tests", () => {
         await page.close();
         await context.close();
     });
+
+describe("authentication", () => {
+    test("Registration with Valid Data", async () => {
+
+    });
+
+    test("Login with Valid Data", async () => {
+
+    });
+
+    test("Logout from the Application", async () => {
+
+    });
+
+});
+
+describe("navbar", () => {
+    test("Navigation for Logged-In User", async () => {
+
+    });
+    test("Navigation for Guest User", async () => {
+
+    });
+    
+});
+
+describe("CRUD", () => {
+    test("Create a Postcart", async () => {
+
+    });
+
+    test("Edit a Postcart", async () => {
+
+    });
+
+    test("Delete a Postcart", async () => {
+        
+    }); 
+});
+
+
 });
