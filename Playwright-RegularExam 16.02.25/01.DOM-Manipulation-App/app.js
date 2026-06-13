1971577a328ede60f9734ea0cad54d7847a9620b
Enver Fransazov <enverfransazov@gmail.com>
2026-06-13 20:11:58 +0300

    first commit of 1 ex.

--- Playwright-RegularExam 16.02.25/01.DOM-Manipulation-App/app.js
+++ Playwright-RegularExam 16.02.25/01.DOM-Manipulation-App/app.js
@@ -1,6 +1,26 @@
 window.addEventListener("load", solve);
 
 function solve() {
+    let roomSize = document.getElementById("room-size");
+    let preferredTimeSlot = document.getElementById("time-slot");
+    let fullName = document.getElementById("full-name");
+    let email = document.getElementById("email");
+    let phoneNumber = document.getElementById("phone-number");
+
+    let previewRoomSize = document.getElementById("preview-room-size");
+    let previewTimeSlot = document.getElementById("preview-time-slot");
+    let previewFullName = document.getElementById("preview-full-name");
+    let previewEmail = document.getElementById("preview-email");
+    let previewPhoneNumber = document.getElementById("preview-phone-number");
+
+    let previewContainer = document.getElementById("preview");
+    let bookRoom = document.getElementById("book-btn");
+
+    bookRoom.addEventListener("click", previewRoom) 
+
+    function previewRoom() {
+        
+    }
 }   
 
     
