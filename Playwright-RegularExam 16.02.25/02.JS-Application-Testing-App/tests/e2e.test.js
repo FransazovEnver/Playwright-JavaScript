d59feeea7b0c2dd001415f9399dba3d3f12697e9
Enver Fransazov <enverfransazov@gmail.com>
2026-06-13 23:39:50 +0300

    second ex. of Exam

---
+++ Playwright-RegularExam 16.02.25/02.JS-Application-Testing-App/tests/e2e.test.js
@@ -0,0 +1,18 @@
+const { test, describe, beforeEach, afterEach, beforeAll, afterAll, expect } = require('@playwright/test');
+const { chromium } = require('playwright');
+
+const host = 'http://localhost:3000/';
+
+let browser;
+let context;
+let page;
+
+let user = {
+    email: "",
+    password: "123456",
+    confirmPassword: "123456",
+};
+
+let albumName = "";
+
+let title = "";
